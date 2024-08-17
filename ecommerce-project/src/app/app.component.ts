7a08b76fb1788dfce3ea00244cde2153b6685814
unknown <elie.c.khoure@gmail.com>
2024-08-18 01:40:41 +0300

    Minor changes

--- ecommerce-project/src/app/app.component.ts
+++ ecommerce-project/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnDestroy, OnInit, signal } from '@angular/core';
-import { AuthenticationService } from './core/auth/services/authentication.service';
 import { Store } from '@ngrx/store';
 import * as fromAuthActions from './core/auth/state/actions/auth.actions';
 import { IProduct } from './features/products/models/Product.model';
-import { ProductsService } from './features/products/services/products.service';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -12,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'ecommerce-project';
 
   products = signal<IProduct[]>([]);
@@ -24,23 +22,18 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private store: Store,
     private router: Router,
-    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.store.dispatch(fromAuthActions.autologin());
 
-    // this.ProductSubscribtion = this.prodService.getProducts().subscribe(products => {
-    //   this.products.set(products);
-    // });
-
     this.router.events.subscribe(() => {
       const currentRoute = this.router.url;
-      this.showFooter = currentRoute !== '/login'; // Modify the path as needed
+      const excludedRoutes = ['/login', '/profile/account-details', '/profile/payment-details'];
+      if (excludedRoutes.includes(currentRoute)) {
+        this.showFooter = false;
+      }
     });
   }
 
-  ngOnDestroy(): void {
-    // this.ProductSubscribtion.unsubscribe();
-  }
 }
